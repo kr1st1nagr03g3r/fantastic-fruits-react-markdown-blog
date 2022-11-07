55c2aecb058cc5581d50c404bbf51cf8ee3b49a5
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-11-07 13:33:39 -0500

    footer, favicon, styling with dracula

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import MediumIcon from '../public/images/global/mediumIcon.svg'
 
 export default function Footer() {
   return (
     <>
       <div className="spacer"></div>
       <div className="footer">
-        <div className="container">
+        <div className="container footer-content">
           <p>
             This is a Static Markdown blog powered by{' '}
             <Link passHref href="https://nextjs.org/">
@@ -22,6 +26,38 @@ export default function Footer() {
             </Link>
             .
           </p>
+          <div className="footer-icons">
+            {' '}
+            <Link
+              href="https://github.com/kr1st1nagr03g3r/fantastic-fruits-react-markdown-blog"
+              passHref
+              alt="fork this repo on github"
+            >
+              <GitHubIcon className="footer-icon" />
+            </Link>{' '}
+            <Link
+              href="
+              https://t.me/+veSYx3FMVPlmMGE5"
+              alt="chat with me on telegram"
+              passHref
+            >
+              <TelegramIcon className="footer-icon" />
+            </Link>{' '}
+            <Link
+              href="
+              https://medium.com/@kr1st1nagr03g3r"
+              alt="Check out my tech writing on Medium"
+              passHref
+            >
+              <Image
+                src={MediumIcon}
+                alt=""
+                height=""
+                width=""
+                className="footer-icon"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ export default function Home({ posts }) {
       <div>
         <Head>
           <title>Fantastic Fruits Markdown Blog Template</title>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍓</text></svg>"
+          />
         </Head>
 
         <div className="posts">
